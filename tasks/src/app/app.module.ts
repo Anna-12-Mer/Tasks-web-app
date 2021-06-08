683a8638fc5feeb478aa577da067277429ee2b6b
Anna-12-Mer <mammeramna@gmail.Com>
2021-06-09 00:30:29 +0200

    add backend

--- tasks/src/app/app.module.ts
+++ tasks/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {CommonModule} from '@angular/common'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    TasksComponent,
+    HeaderComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tasks/src/app/services/task.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Task} from '../models/Task';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private _url = "http://localhost:3000/tasks";
+
+  constructor(private http : HttpClient) { }
+
+  //** Get All Tasks */
+  getTasks ():Observable <Task[]>{
+    return this.http.get<Task[]>(this._url);
+  }
+
+}
